aa56b2d1c70c9787e2940cb7dc83ec7985ed17a1
Odin Linga <odinlinga@gmail.com>
2019-09-24 13:39:16 +0200

    Added a message so i know the program accually runs

--- source/fourone.cpp
+++ source/fourone.cpp
@@ -3,6 +3,7 @@
 #include <IMC/Base/Packet.hpp>
 // see https://github.com/butcherg/DatagramSocket
 #include <Socket/DatagramSocket.h>
+#include <iostream>
 #define LAUVXPLORE1 (30)
 
 using namespace IMC;
@@ -20,4 +21,6 @@ int main()
     Packet::serialize(&abort, (uint8_t *) buffer, size);    //serialize message
     DatagramSocket socket(6002, localhost, true, true);     //create socket
     socket.sendTo(buffer, size, localhost);                 //send message to localhost:6002
+    std::cout << "Program Finished!" << std::endl;
+    return 0;
 }
