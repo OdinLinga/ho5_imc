92a9295d60cd54131272be576dc3b095e5e8feac
Odin Linga <odinlinga@gmail.com>
2019-10-01 16:52:18 +0200

    Commented out unneeded lines from the example

--- source/planspec.cpp
+++ source/planspec.cpp
@@ -1,6 +1,6 @@
-#include <IMC/Spec/Abort.hpp>
-#include <IMC/Base/ByteBuffer.hpp>
-#include <IMC/Base/Packet.hpp>
+//#include <IMC/Spec/Abort.hpp>
+//#include <IMC/Base/ByteBuffer.hpp>
+//#include <IMC/Base/Packet.hpp>
 #include <IMC/Spec/PlanSpecification.hpp>
 #include <IMC/Spec/PlanManeuver.hpp>
 #include <IMC/Spec/Loiter.hpp>
@@ -10,25 +10,22 @@
 #include <IMC/Base/InlineMessage.hpp>
 #include <IMC/Base/Message.hpp>
 // see https://github.com/butcherg/DatagramSocket
-#include <Socket/DatagramSocket.h>
+//#include <Socket/DatagramSocket.h>
 #include <iostream> //For printing
-#define LAUVXPLORE1 (30)
+//#define LAUVXPLORE1 (30)
 
 using namespace IMC;
 
 int main()
 {
-    char localhost[] = "127.0.0.1";
-    Abort abort;                                    //Create Abort message
-    abort.setTimeStamp();                           //set timestamp to current time
-    abort.setDestination(LAUVXPLORE1);              //specify destination
-    int size = abort.getSerializationSize();        //calculate bytes required for serialization
-    char buffer[size];                              //allocate same number of bytes
-
-
-    Packet::serialize(&abort, (uint8_t *) buffer, size);    //serialize message
-    DatagramSocket socket(6002, localhost, true, true);     //create socket
-
+    //char localhost[] = "127.0.0.1";
+    //Abort abort;                                    //Create Abort message
+    //abort.setTimeStamp();                           //set timestamp to current time
+    //abort.setDestination(LAUVXPLORE1);              //specify destination
+    //int size = abort.getSerializationSize();        //calculate bytes required for serialization
+    //char buffer[size];                              //allocate same number of bytes
+    //Packet::serialize(&abort, (uint8_t *) buffer, size);    //serialize message
+    //DatagramSocket socket(6002, localhost, true, true);     //create socket
 
     PlanSpecification ps;
     ps.setTimeStamp(1.5628304294681385E9);
@@ -75,7 +72,7 @@ int main()
     ps.maneuvers = maneuvers;
     ps.toJSON(std::cout);
 
-    socket.sendTo(buffer, size, localhost);                 //send message to localhost:6002
-    std::cout << "Program Finished!" << std::endl;          //Just so i know the program runs.
+    //socket.sendTo(buffer, size, localhost);                 //send message to localhost:6002
+    //std::cout << "Program Finished!" << std::endl;          //Just so i know the program runs.
     return 0;
 }
